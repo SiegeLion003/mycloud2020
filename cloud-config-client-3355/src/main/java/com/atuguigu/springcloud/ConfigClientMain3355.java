64e830ec53e920edac2fe5a18bcc82dba9e50b30
cuihaipeng <you@example.com>
2020-04-06 00:28:02 +0800

    SpringConfig实现,,bus消息总线没写,,stream项目中大量使用不准备写,,在此做记录

---
+++ cloud-config-client-3355/src/main/java/com/atuguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.atuguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Description:
+ * FileName: CloudConfigMain3355
+ * Author: cuihp
+ * Date: 2020/4/5
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
